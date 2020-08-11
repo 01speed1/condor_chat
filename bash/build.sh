53a081eb77a4e75227060095af06169c910ead6e
Oscar Guzman <oscar.guzman@hoytrabajas.com>
2020-08-10 21:25:59 -0500

    from public to build

--- bash/build.sh
+++ bash/build.sh
@@ -3,6 +3,6 @@ npm i
 rm -rf frontend/build
 (cd frontend && yarn install --check-files)
 (cd frontend && yarn build)
-rm -rf public
-mkdir public
-mv frontend/build/* public/
+rm -rf docs
+mkdir docs
+mv frontend/build/* docs/
