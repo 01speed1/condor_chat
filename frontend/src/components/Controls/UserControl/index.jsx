d7a6e4852f73174eb0ea2f87c9fa3e93dc4e54dd
Oscar Guzman <oscar.guzman@hoytrabajas.com>
2020-08-10 21:53:05 -0500

    add photo

--- frontend/src/components/Controls/UserControl/index.jsx
+++ frontend/src/components/Controls/UserControl/index.jsx
@@ -1,33 +1,39 @@
 import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
-import { BACKEND_URL_IMAGES } from "../../../constants";
+import { BACKEND_URL_IMAGES, DEFAULT_IMAGE_PROFILE } from "../../../constants";
 
 function UserControl({ socket, userID }) {
-
-  const [updatedImage, setUpdatedImage] = useState('')
-  const [updatedUsername, setUpdatedUsername] = useState('')
+  const [updatedImage, setUpdatedImage] = useState("");
+  const [updatedUsername, setUpdatedUsername] = useState("");
 
   const avatarURL = `${BACKEND_URL_IMAGES}${updatedImage}`;
 
   useEffect(() => {
-    socket && socket.emit("connectUser", { userID }, ({user: {imagePath, username}}) => {
-
-      setUpdatedImage(imagePath)
-      setUpdatedUsername(username)
-    });
+    socket &&
+      socket.emit(
+        "connectUser",
+        { userID },
+        ({ user: { imagePath, username } }) => {
+          setUpdatedImage(imagePath);
+          setUpdatedUsername(username);
+        }
+      );
   });
 
   return (
     <div className="UserControl">
       <Link to="/dashboard/profile">
-      {updatedImage && (
         <div className="UserControl__avatar">
-          <img src={avatarURL} alt="avatar" />
+          <img
+            src={updatedImage ? avatarURL : DEFAULT_IMAGE_PROFILE}
+            alt="avatar"
+          />
         </div>
-      )}
       </Link>
-      {updatedUsername && <h1 className='UserControl__username' >{`${updatedUsername}`}</h1>}
+      {updatedUsername && (
+        <h1 className="UserControl__username">{`${updatedUsername}`}</h1>
+      )}
       <Link to="/" className="UserControl__closeButton">
         <span className="material-icons">close</span>
       </Link>
